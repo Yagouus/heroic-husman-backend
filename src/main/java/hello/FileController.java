383db7d347abcdde551c79e52b43258f1a06bb70
yagouus <yagofontenla95@gmail.com>
2017-03-27 19:39:47 +0200

    object POST working

--- src/main/java/hello/FileController.java
+++ src/main/java/hello/FileController.java
@@ -1,16 +1,19 @@
 package hello;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import hello.storage.StorageService;
 
@@ -27,12 +30,6 @@ public class FileController {
         this.storageService = storageService;
     }
 
-    @RequestMapping("/file")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
-    }
-
     @GetMapping("/archivos")
     public List<String> listUploadedFiles(Model model) throws IOException {
 
@@ -45,4 +42,25 @@ public class FileController {
                 .collect(Collectors.toList());
 
     }
+
+    @RequestMapping(value="/imageUpload", method = RequestMethod.POST)
+    public void UploadFile(MultipartHttpServletRequest request) throws IOException {
+
+        Iterator<String> itr=request.getFileNames();
+        MultipartFile file=request.getFile(itr.next());
+        String fileName=file.getOriginalFilename();
+        File dir = new File("C:\\file");
+        if (dir.isDirectory())
+        {
+            File serverFile = new File(dir,fileName);
+            BufferedOutputStream stream = new BufferedOutputStream(
+                    new FileOutputStream(serverFile));
+            stream.write(file.getBytes());
+            stream.close();
+        }else {
+            System.out.println("not");
+        }
+
+    }
 }
+
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,9 +1,9 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class GreetingController {
@@ -13,7 +13,15 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        System.out.println(name);
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+
+    @RequestMapping(value = "/complexGreeting", method = RequestMethod.POST)
+    public void complexGreeting(complexGreeting g) {
+        System.out.println(g.getContent());
+        System.out.println(g.getAge());
+    }
 }
+
